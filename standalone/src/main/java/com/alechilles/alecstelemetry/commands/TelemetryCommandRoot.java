3b5caead7c48c37b7a9313586d9128c68920a256
Alechilles <22alec22@gmail.com>
2026-05-28 11:45:29 -0400

    Fix: use stable telemetry command permission

--- standalone/src/main/java/com/alechilles/alecstelemetry/commands/TelemetryCommandRoot.java
+++ standalone/src/main/java/com/alechilles/alecstelemetry/commands/TelemetryCommandRoot.java
@@ -9,9 +9,12 @@ import javax.annotation.Nonnull;
  * Root `/telemetry` command dispatcher.
  */
 public final class TelemetryCommandRoot extends AbstractCommandCollection {
+    public static final String ROOT_PERMISSION = "telemetry.command.telemetry";
 
     public TelemetryCommandRoot(@Nonnull AlecsTelemetry plugin) {
         super("telemetry", "Alec's Telemetry commands.");
+        requirePermission(ROOT_PERMISSION);
+        setPermissionGroups("OP", "Admin", "Operator");
         addSubCommand(new TelemetryStatusCommand(plugin));
         addSubCommand(new TelemetryProjectsCommand(plugin));
         addSubCommand(new TelemetryProjectCommand(plugin));
